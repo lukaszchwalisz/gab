72586f2afa8aa2e6978a636b73104f97909ad9b7
Łukasz Chwalisz <lukasz.chwalisz@gmail.com>
2024-08-11 23:53:54 +0200

    clarification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
       priority
     />
 
-    <h2 className="m-4 text-4xl font-bold font-mono text-orange-500">O MNIE</h2>
+    <h2 className="m-4 font-bold font-mono text-orange-500">O MNIE</h2>
     <div className="prose prose-base m-4 text-justify font-mono border-dotted border-2 border-orange-500 p-4">
 
       <p>Cześć! Nazywam się <span className="font-bold text-orange-500">Łukasz Chwalisz</span>. Jestem psychologiem, absolwentem Uniwersytetu im. Adama Mickiewicza w Poznaniu, Wydział Nauk Społecznych, Instytut Psychologii (obecnie Wydział Psychologii i Kogniwistyki UAM) (2006 r.). Pracuję jako psychoterapeuta. Chciałbym zaprosić Państwa do zapoznania się z ofertę mojej pracy w ramach prywatnego gabnetu psychologicznego.
@@ -42,14 +42,14 @@ export default function Home() {
       <p>Od 2022 odbywam szkolenie w ramach terapii systemowej organizowane przez Wielkopolskie Towarzystwo Terapii Systemowej. Dotychczas ukończyłem kurs podstawowy. Uczestniczę w superwizji grupowej w ramach podejścia systemowego.</p>
     </div>
 
-    <h2 className="m-4 text-4xl font-bold font-mono text-orange-500">OFERTA</h2>
-    <h2 className="m-4 text-2xl font-bold font-mono text-gray-500 bg-yellow-200">Psychoterapia</h2>
+    <h2 className="m-4 font-bold font-mono text-orange-500">OFERTA</h2>
+    <h3 className="m-4 font-bold font-mono text-gray-500 bg-yellow-200">Psychoterapia</h3>
 
     <div className="container lg:flex lg:space-x-4 m-4 p-4 font-mono text-justify">
       <div className="grid gap-4 mt-auto justify-center m-4 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-3">
 
         <div className="p-4 mb-4">
-        <h3 className="text-lg text-center font-bold mb-4">PSYCHOTERAPIA INDYWIDUALNA</h3>
+        <h2 className="text-center font-bold mb-4">PSYCHOTERAPIA INDYWIDUALNA</h2>
         <div className="">
         <p>Psychoterapia osobowości, analiza treści nieświadomych, mechanizmów obronych, symboliki zawartej w snach, a także funkcjonowania w systemie rodzinnym, analiza genogramu, motywów postępowania wewnętrznęgo i zewnętrznego na różnych poziomach.</p>
         <p className="font-semibold text-orange-500 text-right mt-2">150 zł / 50 minut</p>
@@ -57,13 +57,13 @@ export default function Home() {
         </div>
 
         <div className="p-4 mb-4">
-        <h3 className="text-lg text-center font-bold mb-4">PSYCHOTERAPIA INDYWIDUALNA (ONLINE)</h3>
+        <h2 className="text-center font-bold mb-4">PSYCHOTERAPIA INDYWIDUALNA (ONLINE)</h2>
         <p>Psychoterapia indywidualna w formule online może być uzupełnieniem do procesu psychoterapii stacjonalnej albo osobnym procesem terapeutycznym. Ze względu na specyfikę w/w formy kontaktu z pacjentem zalecam okresowe wizyty w formie psychoterapii stacjonarnej.</p>
         <p className="font-semibold text-orange-500 text-right  mt-2">150 zł / 50 minut</p>
         </div>
 
         <div className="p-4 mb-4">
-        <h3 className="text-lg text-center font-bold mb-4">TERAPIA PAR</h3>
+        <h2 className="text-center font-bold mb-4">TERAPIA PAR</h2>
         <p>Psychoterapia pary, mediacje rodzinne - poprawa wzajemnego zrozumienia potrzeb, komunikacji w parze, wypracowanie konstruktywnych rozwiązań bądź przepracowanie rozstania, relacje z dziećmi, funcjonowanie rodziny w sytuacji kryzysu małżeńskiego
         </p>
         <p className="font-semibold text-orange-500 text-right mt-2">200 zł / 80 minut</p>
@@ -72,7 +72,7 @@ export default function Home() {
       </div>
     </div>
 
-      <h2 className="m-4 text-2xl font-bold font-mono text-gray-500 bg-yellow-200">Warsztaty</h2>
+      <h2 className="m-4 font-bold font-mono text-gray-500 bg-yellow-200">Warsztaty</h2>
 
 
     <div className="container lg:flex lg:space-x-4 m-4 p-4 font-mono text-justify">
@@ -123,7 +123,7 @@ export default function Home() {
     </div>
 
 
-    <h2 className="m-4 text-4xl font-bold font-mono text-orange-500">BLOG</h2>
+    <h2 className="m-4 font-bold font-mono text-orange-500">BLOG</h2>
 
 
     <div className="container lg:flex lg:space-x-4 m-4 p-4 font-mono text-justify">
@@ -133,7 +133,7 @@ export default function Home() {
           <CarouselItem className="md:basis-1/2 lg:basis-1/3">
           <div className="p-4 mb-4 space-x-4">
 
-          <h3 className="text-lg text-center font-bold mb-4">Pacjent Zero</h3>
+          <h3 className="text-center font-bold mb-4">Pacjent Zero</h3>
           
           <p className="text-gray-500">Rok 2020. Człowiek w masce idący uliczkami miast, trzymający się w odpowiedniej odległości od innych osób, które także mają zakryte twarze. Każdy dzień niesie za sobą informacje o nowych zakażonych, wyleczonych, zmarłych.</p>
           </div>
@@ -141,21 +141,21 @@ export default function Home() {
 
           <CarouselItem className="md:basis-1/2 lg:basis-1/3">
           <div className="p-4 mb-4 space-x-4">
-          <h3 className="text-lg text-center font-bold mb-4">Dwa filmy</h3>
+          <h3 className="text-center font-bold mb-4">Dwa filmy</h3>
           <p className="text-gray-500">Pod koniec 2019 roku miały miejsce premiery dwóch filmów, bardzo ciekawych, w pewien sposób podobnych do siebie i bardzo różnych zarazem. Mam na myśli "Dwóch papieży", wyprodukowany przez Netflix oraz wyświetlany w kinach studyjnych "The Lighthouse".</p>
           </div>
           </CarouselItem>
 
           <CarouselItem className="md:basis-1/2 lg:basis-1/3">
           <div className="p-4 mb-4 space-x-4">
-          <h3 className="text-lg text-center font-bold mb-4">Nieświadomość indywidualna i zbiorowa - wystąpienie na kongresie "Obrazy nieświadomości – jungowskie metody pracy z psychiką 6.10.2019 r."</h3>
-          <p className="text-gray-500">Dzień dobry. W godzinach popołudniowych zaprezentujemy Państwu warsztat „Symbolic constellations” autorstwa czeskiego analityka jungowskiego Martina Skali, który w założeniach teoretycznych odwołuje się bezpośrednio do koncepcji nieświadomości indywidualnej i nieświadomości zbiorowej.</p>
+          <h3 className="text-center font-bold mb-4">Nieświadomość indywidualna i zbiorowa</h3>
+          <p className="text-gray-500">Wystąpienie na kongresie "Obrazy nieświadomości – jungowskie metody pracy z psychiką 6.10.2019 r.</p>
           </div>
           </CarouselItem>
 
           <CarouselItem className="md:basis-1/2 lg:basis-1/3">
           <div className="p-4 mb-4 space-x-4">
-          <h3 className="text-lg text-center font-bold mb-4">Golem: archetypowy mit czy miejska legenda?</h3>
+          <h3 className="text-center font-bold mb-4">Golem: archetypowy mit czy miejska legenda?</h3>
           <p className="text-gray-500">Pod koniec sesji pacjentka spytała mnie o figurkę, która znajdowała się w moim gabinecie. Figurka przedstawiała słonia, który nosił na sobie zegar.</p>
 
 
@@ -164,7 +164,7 @@ export default function Home() {
 
           <CarouselItem className="md:basis-1/2 lg:basis-1/3">
           <div className="p-4 mb-4 space-x-4">
-          <h3 className="text-lg text-center font-bold mb-4">Sny a nieświadomy sens</h3>
+          <h3 className="text-center font-bold mb-4">Sny a nieświadomy sens</h3>
           <p className="text-gray-500">W rozdziale „Podróże” we książce „Wspomnienia, sny, myśli” C.G. Jung przytoczył sen, który miał podczas podróży do Tunisu.</p>
 
 
@@ -173,7 +173,7 @@ export default function Home() {
 
           <CarouselItem className="md:basis-1/2 lg:basis-1/3">
           <div className="p-4 b-4 space-x-4">
-          <h3 className="text-lg text-center font-bold mb-4">Co oznacza Lechaim ?</h3>
+          <h3 className="text-center font-bold mb-4">Co oznacza Lechaim ?</h3>
           <p className="text-gray-500">„Lechaim !” To tradycyjny toast przed piciem wina czy innego napoju, oznaczający dosłownie „Za życie!”.
           </p>
 
@@ -187,7 +187,7 @@ export default function Home() {
     </Carousel>
     </div>
 
-    <h2 className="m-4 text-4xl font-bold font-mono text-orange-500">KONTAKT</h2>
+    <h2 className="m-4 font-bold font-mono text-orange-500">KONTAKT</h2>
     <div className="prose prose-base m-4 text-justify font-mono border-dotted border-2 border-orange-500 p-4">
       <p>tel. 665 696 372</p>
       <p>lukasz.chwalisz@gmail.com</p>
